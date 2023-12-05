ec1f07c9129ea805f37270b4437cd3899d1f0342
简讯Alfred <jianxun2004@gmail.com>
2023-12-05 20:00:24 +0800

    nvim-tree 自动关闭

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,9 +2,18 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- 保存时自动格式化代码
 autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+    pattern = "*",
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
 })
 
+-- nvim-tree 自动关闭
+autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+            vim.cmd("quit")
+        end
+    end,
+})
