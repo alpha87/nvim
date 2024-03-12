7284f0769a7aa5dbffe5b0783d8a89694e3d4f5a
简讯Alfred <jianxun2004@gmail.com>
2024-03-12 11:27:52 +0800

    更新debug图标

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,6 +2,67 @@ return {
     {
         "mfussenegger/nvim-dap",
         config = function()
+            local dap_breakpoint_color = {
+                breakpoint = {
+                    ctermbg = 0,
+                    fg = "#993939",
+                    bg = "#31353f",
+                },
+                logpoing = {
+                    ctermbg = 0,
+                    fg = "#61afef",
+                    bg = "#31353f",
+                },
+                stopped = {
+                    ctermbg = 0,
+                    fg = "#98c379",
+                    bg = "#31353f",
+                },
+            }
+
+            vim.api.nvim_set_hl(0, "DapBreakpoint", dap_breakpoint_color.breakpoint)
+            vim.api.nvim_set_hl(0, "DapLogPoint", dap_breakpoint_color.logpoing)
+            vim.api.nvim_set_hl(0, "DapStopped", dap_breakpoint_color.stopped)
+
+            local dap_breakpoint = {
+                error = {
+                    text = "",
+                    texthl = "DapBreakpoint",
+                    linehl = "DapBreakpoint",
+                    numhl = "DapBreakpoint",
+                },
+                condition = {
+                    text = "",
+                    texthl = "DapBreakpoint",
+                    linehl = "DapBreakpoint",
+                    numhl = "DapBreakpoint",
+                },
+                rejected = {
+                    text = "✘",
+                    texthl = "DapBreakpint",
+                    linehl = "DapBreakpoint",
+                    numhl = "DapBreakpoint",
+                },
+                logpoint = {
+                    text = "",
+                    texthl = "DapLogPoint",
+                    linehl = "DapLogPoint",
+                    numhl = "DapLogPoint",
+                },
+                stopped = {
+                    text = "",
+                    texthl = "DapStopped",
+                    linehl = "DapStopped",
+                    numhl = "DapStopped",
+                },
+            }
+
+            vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
+            vim.fn.sign_define("DapBreakpointCondition", dap_breakpoint.condition)
+            vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
+            vim.fn.sign_define("DapLogPoint", dap_breakpoint.logpoint)
+            vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
+
             local dap = require("dap")
             dap.adapters.python = {
                 type = "executable",
@@ -28,15 +89,6 @@ return {
             require("dapui").setup({
                 {
                     icons = { expanded = "", collapsed = "", current_frame = "" },
-                    mappings = {
-                        -- Use a table to apply multiple mappings
-                        expand = { "<CR>", "<2-LeftMouse>" },
-                        open = "o",
-                        remove = "d",
-                        edit = "e",
-                        repl = "r",
-                        toggle = "t",
-                    },
                     layouts = {
                         {
                             elements = {
