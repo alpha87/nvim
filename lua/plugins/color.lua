d09f14b87353a413204006d966577277bff824cd
简讯Alfred <jianxun2004@gmail.com>
2024-03-12 10:41:03 +0800

    修改主题

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,12 +1,12 @@
 return {
     {
-        "Everblush/nvim",
-        name = "everblush",
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             -- load the colorscheme here
-            vim.cmd([[colorscheme everblush]])
+            vim.cmd([[colorscheme kanagawa]])
         end,
     },
 }
