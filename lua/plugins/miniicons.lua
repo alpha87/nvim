6eb79429e239034552c37b6a9bc03e2d352235ec
简讯Alfred <jianxun2004@gmail.com>
2024-09-30 11:51:05 +0800

    配置 mini icons

--- lua/plugins/miniicons.lua
+++ lua/plugins/miniicons.lua
@@ -1,4 +1,11 @@
 return {
     { "echasnovski/mini.nvim", version = false },
-    { "echasnovski/mini.icons", version = false },
+    {
+        "echasnovski/mini.icons",
+        config = function()
+            require("mini.icons").setup({
+                style = "ascii",
+            })
+        end,
+    },
 }
