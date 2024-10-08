02e97979ee4239b59209d9f40c0b2978dde125b5
简讯Alfred <jianxun2004@gmail.com>
2024-10-08 16:10:42 +0800

    优化代码格式

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -10,48 +10,48 @@ return {
 
             configs.setup({
                 ensure_installed = {
-		"bash",
-            "c",
-            "diff",
-            "html",
-            "javascript",
-            "jsdoc",
-            "json",
-            "jsonc",
-            "lua",
-            "luadoc",
-            "luap",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "query",
-            "regex",
-            "toml",
-            "tsx",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "yaml"
-    },
-				indent = {
-            enable = true
-        },
-        -- p00f/nvim-ts-rainbow
-        rainbow = {
-            enable = true,
-            -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-            extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-            max_file_lines = nil, -- Do not enable for files with more than n lines, int
-            colors = {
-                "#95ca60",
-                "#ee6985",
-                "#D6A760",
-                "#7794f4",
-                "#b38bf5",
-                "#7cc7fe"
-            } -- table of hex strings
-            -- termcolors = { } -- table of colour name strings
-        },
+                    "bash",
+                    "c",
+                    "diff",
+                    "html",
+                    "javascript",
+                    "jsdoc",
+                    "json",
+                    "jsonc",
+                    "lua",
+                    "luadoc",
+                    "luap",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "query",
+                    "regex",
+                    "toml",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "vimdoc",
+                    "yaml",
+                },
+                indent = {
+                    enable = true,
+                },
+                -- p00f/nvim-ts-rainbow
+                rainbow = {
+                    enable = true,
+                    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+                    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+                    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+                    colors = {
+                        "#95ca60",
+                        "#ee6985",
+                        "#D6A760",
+                        "#7794f4",
+                        "#b38bf5",
+                        "#7cc7fe",
+                    }, -- table of hex strings
+                    -- termcolors = { } -- table of colour name strings
+                },
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
